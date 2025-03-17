1a4325777c065609f3723e41e59d52090f01a731
ossema90 <d.ossema.daoud@gmail.com>
2025-03-17 09:41:02 +0100

    refactor: update contact page to use IonicModule and integrate SendMessageComponent

--- src/app/pages/contact/contact.page.ts
+++ src/app/pages/contact/contact.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {IonicModule} from '@ionic/angular';
 import { SendMessageComponent } from 'src/app/components/ui/send-message/send-message.component';
 
 @Component({
@@ -10,10 +10,7 @@ import { SendMessageComponent } from 'src/app/components/ui/send-message/send-me
   styleUrls: ['./contact.page.scss'],
   standalone: true,
   imports: [
-    IonContent,
-    IonHeader,
-    IonTitle,
-    IonToolbar,
+    IonicModule,
     CommonModule,
     FormsModule,
     SendMessageComponent
