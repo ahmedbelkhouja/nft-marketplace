5282ea8370dc6f9d83b9787d6a27fd9f4f43b325
ossema90 <d.ossema.daoud@gmail.com>
2025-03-18 09:58:51 +0100

    feat: replace SendMessage component with inline form in Contact page

--- src/app/pages/connect/connect.page.ts
+++ src/app/pages/connect/connect.page.ts
@@ -1,22 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-import { WalletComponent } from 'src/app/components/ui/wallet/wallet.component';
+
+import { CommonSectionComponent } from '../../components/ui/common-section/common-section.component';
+
+interface WalletData {
+  title: string;
+  desc: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-connect',
   templateUrl: './connect.page.html',
   styleUrls: ['./connect.page.scss'],
-  standalone: true,
-  imports: [CommonModule, FormsModule, IonicModule, WalletComponent]
+  imports: [CommonModule, FormsModule, IonicModule,CommonSectionComponent],
 })
-export class ConnectPage implements OnInit {
 
-  constructor() { }
+export class ConnectPage {
+  walletdata: WalletData[] = [
+    {
+      title: 'Bitcoin',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium accusamus repellat rerum consequatur explicabo? Reiciendis!',
+      icon: 'ri-bit-coin-line',
+    },
 
-  ngOnInit() {
-  }
+    {
+      title: 'Coinbase',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium accusamus repellat rerum consequatur explicabo? Reiciendis!',
+      icon: 'ri-coin-line',
+    },
 
+    {
+      title: 'Metamask',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium accusamus repellat rerum consequatur explicabo? Reiciendis!',
+      icon: 'ri-money-cny-circle-line',
+    },
+
+    {
+      title: 'Authereum',
+      desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium accusamus repellat rerum consequatur explicabo? Reiciendis!',
+      icon: 'ri-bit-coin-line',
+    },
+  ];
 }
