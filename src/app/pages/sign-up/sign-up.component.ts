1436a4dfd9973daabec0ccca27791652bcee65f9
Ahmed Belkhouja <belkhoua.ahmed@gmail.com>
2025-03-24 10:48:14 +0100

    feat: implement sign-up form with validation and profile picture upload, update routing for market details

--- src/app/pages/sign-up/sign-up.component.ts
+++ src/app/pages/sign-up/sign-up.component.ts
@@ -1,14 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss'],
+  standalone: true,
+  imports: [ReactiveFormsModule, IonicModule, FormsModule, CommonModule],
 })
-export class SignUpComponent  implements OnInit {
+export class SignUpComponent {
 
-  constructor() { }
+  signUpForm: FormGroup;
+  previewUrl: string | null = null;
 
-  ngOnInit() {}
+  constructor() {
+    this.signUpForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      profilePicture: new FormControl(null)
+    });
+  }
 
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      if (file.size > 5000000) {
+        alert('File size should not exceed 5MB');
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.previewUrl = e.target.result;
+        this.signUpForm.patchValue({
+          profilePicture: file
+        });
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      console.log('Form Submitted:', this.signUpForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
