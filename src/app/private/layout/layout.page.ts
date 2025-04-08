520974ba6e011ee3ab38f3baf29120f8318e3b75
ossema90 <d.ossema.daoud@gmail.com>
2025-04-08 10:21:32 +0100

    Add footer component with layout and styling; integrate into layout page

---
+++ src/app/private/layout/layout.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+import { FooterComponent } from '../footer/footer.component';
+
+@Component({
+  selector: 'app-layout',
+  templateUrl: './layout.page.html',
+  styleUrls: ['./layout.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, RouterModule, FooterComponent],
+})
+export class LayoutPage implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
