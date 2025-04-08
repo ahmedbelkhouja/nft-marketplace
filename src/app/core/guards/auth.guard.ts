51f3b78d013ae05b94134f04e2d914f30e945c97
Ahmed Belkhouja <belkhoua.ahmed@gmail.com>
2025-04-08 16:44:21 +0100

    Implement authentication guard and update routing; refactor auth service to return observable

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -1,13 +1,22 @@
-import { AuthService } from '../services/auth.service';
+// auth.guard.ts
+import { CanActivateFn } from '@angular/router';
 import { inject } from '@angular/core';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import {
-  Router,
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot,
-  GuardResult,
-  MaybeAsync,
-} from '@angular/router';
-import { catchError, map, of } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { map } from 'rxjs/operators';
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService);
+  const router = inject(Router);
+
+  return authService.isAuthenticated().pipe(
+    map((res) => {
+      if (res.isAuthenticated) {
+        return true; // Allow access
+      } else {
+        router.navigate(['public/login']); // Redirect to login if not authenticated
+        return false;
+      }
+    })
+  );
+};
