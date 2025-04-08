3bb0ebefa2e373e6168d6d5809c5a244414141df
ossema90 <d.ossema.daoud@gmail.com>
2025-04-08 10:14:00 +0100

    Add history page with table layout and styling; refactor routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,30 +3,18 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    redirectTo: 'public', // Keep only one redirect
+    pathMatch: 'full',
+  },
+
+  {
+    path: 'public',
     loadChildren: () =>
       import('./public/app.public.routes').then((m) => m.publicRoutes),
   },
   {
     path: 'private',
     loadChildren: () =>
-      import('./private/app.private.routes').then(
-        (m) => m.privateRoutes // Load private routes
-      ),
-  },  {
-    path: 'login',
-    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+      import('./private/app.private.routes').then((m) => m.privateRoutes),
   },
-  {
-    path: 'nft-details',
-    loadComponent: () => import('./pages/nft-details/nft-details.page').then( m => m.NftDetailsPage)
-  },
-  {
-    path: 'sign-up',
-    loadComponent: () => import('./pages/sign-up/sign-up.page').then( m => m.SignUpPage)
-  },
-  {
-    path: 'history',
-    loadComponent: () => import('./pages/history/history.page').then( m => m.HistoryPage)
-  },
-
 ];
