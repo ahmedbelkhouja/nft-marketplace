8c46d71acef752508ddef444b1291c250edb911a
ossema90 <d.ossema.daoud@gmail.com>
2025-04-08 10:56:42 +0100

    Refactor hero component: move to new location, update imports, and remove history component

--- src/app/shared/components/ui/seller-card/seller-card.component.spec.ts
+++ src/app/shared/components/ui/seller-card/seller-card.component.spec.ts
@@ -1,21 +1,6 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
-<<<<<<<< HEAD:src/app/components/ui/common-section/common-section.component.spec.ts
-import { CommonSectionComponent } from './common-section.component';
-
-describe('CommonSectionComponent', () => {
-  let component: CommonSectionComponent;
-  let fixture: ComponentFixture<CommonSectionComponent>;
-
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CommonSectionComponent ],
-      imports: [IonicModule.forRoot()]
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(CommonSectionComponent);
-========
 import { SellerCardComponent } from './seller-card.component';
 
 describe('SellerCardComponent', () => {
@@ -24,12 +9,11 @@ describe('SellerCardComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ SellerCardComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [SellerCardComponent],
+      imports: [IonicModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(SellerCardComponent);
->>>>>>>> master:src/app/components/ui/seller-card/seller-card.component.spec.ts
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
